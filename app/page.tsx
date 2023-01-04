9cc3375038046d4c20a946d3ba9649bd8961f7d9
AndySmithQA <andy.smtih@qa.com>
2023-01-04 16:08:16 +0000

    Fist Commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+
+import React from "react";
+
+function Home() {
+    return(
+        <div className='text-red-500'>I am the Home</div>
+    )
+}
+
+export default Home
